8fb82a04586e387b121e506be7dcd53d1f2a56d3
HyeonjiGWON <khj0523527@gmail.com>
2018-11-22 23:41:22 +0900

    datastructure

---
+++ Linked-list/linkmain.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+#pragma warning (disable:4996)
+
+int main(void)
+{
+	int* newData;
+	int* nodeData;
+	NODE* node;
+
+	newData = (int*)malloc(sizeof(int));
+	*newData = 7;
+	node = createNode(newData);
+	newData = (int*)malloc(sizeof(int));
+	*newData = 75;
+	node->link = createNode(newData);
+	nodeData = (int*)node->dataPtr;
+	printf("Data from node 1: %d\n", *nodeData);
+	nodeData = (int*)node->link->dataPtr;
+	printf("Data from node 2: %d\n", *nodeData);
+	return 0;
+}
