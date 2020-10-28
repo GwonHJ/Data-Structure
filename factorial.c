d6501d0d6bcc90b57d079abf01304f5d572f68f8
GwonHJ <45057466+GwonHJ@users.noreply.github.com>
2020-10-29 03:05:40 +0900

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int fact(int);
+
+// recursion 방식의 factorial 구하기
+int fact(int);//프로토타입선언
 
 int main(void) {
 	int n = 5;
 	for (int i = 1; i <= n; i++) {
 	if (i < n) {
-		printf("%d != %d\n", i, fact(i));
+		printf("%d != %d\n", i, fact(i));//1!~4!까지의 값 출력
 	}
 	else if(i==n)
-		printf("fact(%d)= %d\n", n,fact(5));
+		printf("fact(%d)= %d\n", n,fact(5));//5!의 값 출력
 }
 }
 
@@ -18,4 +20,4 @@ int fact(int n)
 {
 	if (n <= 1) return 1;
 	else return n*fact(n - 1);
-}
+}// factorial 함수
